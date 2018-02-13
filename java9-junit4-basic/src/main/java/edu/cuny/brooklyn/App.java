c54d81807b15e0cdca28512ca047e322ec55f0c9
huichen-cs <chen@sci.brooklyn.cuny.edu>
2018-02-13 09:40:41 -0500

    added a Maven archetype

---
+++ java9-junit4-basic/src/main/java/edu/cuny/brooklyn/App.java
@@ -0,0 +1,11 @@
+package edu.cuny.brooklyn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ java9-junit4-basic/target/generated-sources/archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
