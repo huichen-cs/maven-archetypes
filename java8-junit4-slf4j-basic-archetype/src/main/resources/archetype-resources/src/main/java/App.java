d83a2c6098a01acb73a3636d5804bc46534edc11
huichen-cs <chen@sci.brooklyn.cuny.edu>
2018-02-13 17:00:11 -0500

    added more archetypes

---
+++ java8-junit4-slf4j-basic-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private final static Logger LOGGER = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info("Hello World!");
+	}
+}
